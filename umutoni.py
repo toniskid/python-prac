2071105a2031fd9a89d9fde27bc8199dc1f389f7
toniskid <165775876+toniskid@users.noreply.github.com>
2026-09-03 07:39:52 +0300

    add the subjects and letter grades

--- umutoni.py
+++ umutoni.py
@@ -1,5 +1,19 @@
 print("My name umutoni")
 print("I am a tech student")
 print(" I love poetry")
-print(" My shoe size isd 41")
+print(" My shoe size is 41")
 print("My favourite fruit is an apple")
+name = "umutoni"
+subject1 = "python"
+subject2 = "Networking"
+subject3 = "database"
+subject4 = "Cybersecurity"
+grade1 = "A"
+grade2 = "B"
+grade3 = "C"
+grade4 = "D"
+print("Name:", name)
+print("Subject 1:", subject1, "Grade:", grade1)
+print("Subject 2:", subject2, "Grade:", grade2)
+print("Subject 3:", subject3, "Grade:", grade3)
+print("Subject 4:", subject4, "Grade:", grade4)
